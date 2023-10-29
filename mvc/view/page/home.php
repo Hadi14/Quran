dc4eae327606eaa15bddbc70ca16544fefad31ae
Hadi14 <asusn43@yahoo.com>
2023-10-29 13:31:41 +0330

    optim aye number and sign

--- mvc/view/page/home.php
+++ mvc/view/page/home.php
@@ -2,14 +2,17 @@
     <?
     foreach ($data as $key => $value) {
         echo "<span>" . $value['Arabic'] . "</span>";
-        echo "<span> ‎﴿ </span>";
+        echo "<span class='ayesign'>﴿</span>";
         echo  "<span class='ayeNum'>" . $value['Verse'] . "</span>";
-        echo  "<span>﴾</span>";
+        echo  "<span class='ayesign'>﴾ </span>";
     }
     ?>
 </div>
 
 <script>
-    n = $('span').text().replace(/(بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ)/g, `<div class="center">$1 </div>`);
+    n = $('span').innerHTML().replace(/(بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ)/g, `<div class="center">$1 </div>`);
+    // n = n.replace(/(﴿)/g, "<span>﴿</span>");
+    // n = n.replace(/[1-9]|[1-)/g, "<span>﴿</span>");
+    // n = n.replace(/(﴾ )/g, "<span>﴾ </span>");
     document.getElementById("divmain").innerHTML = n;
 </script>
--- theme/default.php
+++ theme/default.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>قرآن آنلاین</title>
-    <link rel="stylesheet" href="<?= getBaseUrl() ?>css/style.css">
     <link rel="stylesheet" href="<?= getBaseUrl() ?>css/base.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="<?= getBaseUrl() ?>js/jquery-3.7.0.js"></script>
+    <link rel="stylesheet" href="<?= getBaseUrl() ?>css/style.css">
 </head>
 
 <body>

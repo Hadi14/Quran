6160e5bca0c163720742608c6714db784a96b194
“Hadi14” <asusn43@yahoo.com>
2023-10-30 20:41:58 +0330

    کار روی قائده ادغام

--- mvc/view/page/home.php
+++ mvc/view/page/home.php
@@ -13,7 +13,18 @@
 
 <script>
     $(document).ready(function() {
-        // document.write("حَرْفٌ".replace(/(\u0652)|(\u0650)|(\u064C)|(\u064E)|(\u064B)|(\u064F)|(\u064D)|(\u0651)/g,"")); مهم مهم مهم  - اعراب حروف عربی
+        // u0652 --> علامت سکون
+        // u0646 --> نون ساکن
+
+        // u064E --> فتحه
+        // u0650 --> کسره
+        // u064F --> ُ   ضمه  اُ
+
+        // u064D --> ٍ   اِن
+        // u064C --> ٌ   اُن
+        // u064B --> ً  اَن
+
+        // u 0651 --> تشدید
         let n = $('span').text().replace(/(بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ)/g, "<div class='center'>$1 </div>"); //++ این میتونه بهینه بشه که سه خط زیر نوشته نشه
         // n = n.replace(/(\u064D)/g, "<span class='redcolor'>$1</span>"); // تنوین   ٍ
         // n = n.replace(/(\u0646)/g, "<span class='redcolor'>$1</span>"); //حرف ن
@@ -21,10 +32,12 @@
         n = n.replace(/([0-9])/g, "<span class='ayeNum'>$1</span>");
         n = n.replace(/(﴾ )/g, "<span class='ayesign'>﴾ </span>");
 
-        n = n.replace(/(\u064B|\u064C|\u064D|)(\u0646)/g, "<span class='redcolor'>$2</span>");
+        n = n.replace(/(\u064B|\u064C|\u064D|\u0646\u0652)(\s\u0646)/g, "<span class='redcolor'>$1$2</span>");
+        // n = n.replace(/(\u0646\u0652)(\s\u0646)/g, "<span class='redcolor'>$1$2</span>");
 
 
         document.getElementById("divmain").innerHTML = n;
+
 
     });
 </script>
